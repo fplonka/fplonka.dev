3313b243cde3ce669f2bd412838511ecfeca6f66
Filip Płonka <github@1f389.com>
2023-11-24 14:22:26 +0100

    Make separate folders, add bpfreq readme

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	http.HandleFunc("/bpfreq", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/bpfreq.html")
+		http.ServeFile(w, r, "./static/bpfreq/bpfreq.html")
 	})
 
 	http.HandleFunc("/go-llca", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/go-llca.html")
+		http.ServeFile(w, r, "./static/go-llca/go-llca.html")
 	})
 
 	http.ListenAndServe(":3001", nil)
