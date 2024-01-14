b02ce4516414d2606dabb7ccbd629a893e6be3d5
Filip Płonka <github@1f389.com>
2024-01-14 19:51:03 +0100

    Add git-cluster

--- main.go
+++ main.go
@@ -16,5 +16,12 @@ func main() {
 		http.ServeFile(w, r, "./static/go-llca/go-llca.html")
 	})
 
+	http.HandleFunc("/git-cluster", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/git-cluster/git-cluster.html")
+	})
+
+	// Serve files from git-cluster/output
+	http.Handle("/git-cluster/output/", http.StripPrefix("/git-cluster/output/", http.FileServer(http.Dir("./static/git-cluster/output"))))
+
 	http.ListenAndServe(":3001", nil)
 }
