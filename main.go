aa810c3fa04b6095fe3ab83004bc8ed5f6f14354
Filip Płonka <github@1f389.com>
2024-08-03 00:19:14 +0200

    add organize-anything 1 mil wikipedia articles demo

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -19,9 +22,29 @@ func main() {
 	http.HandleFunc("/git-cluster", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/git-cluster/git-cluster.html")
 	})
-
 	// Serve files from git-cluster/output
 	http.Handle("/git-cluster/output/", http.StripPrefix("/git-cluster/output/", http.FileServer(http.Dir("./static/git-cluster/output"))))
 
+	http.HandleFunc("/organize-anything", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/organize-anything/skeleton.html")
+	})
+	http.HandleFunc("/fragment/", serveFragment)
+
 	http.ListenAndServe(":3001", nil)
+}
+
+// Handler to serve fragment files
+func serveFragment(w http.ResponseWriter, r *http.Request) {
+	fragmentID := filepath.Base(r.URL.Path)
+	filename := filepath.Join("static/organize-anything/", fragmentID+".html")
+
+	fmt.Println("WANT:", fragmentID)
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		http.NotFound(w, r)
+		fmt.Println("not found...")
+		return
+	}
+
+	http.ServeFile(w, r, filename)
 }
